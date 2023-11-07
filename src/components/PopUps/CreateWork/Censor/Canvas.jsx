b6e2d3cef93bd5a2ddf105e45d0c9e0f9be1280f
reworldcreative <reworldcreative@gmail.com>
2023-11-07 16:21:13 +0200

    canvas test

--- src/components/PopUps/CreateWork/Censor/Canvas.jsx
+++ src/components/PopUps/CreateWork/Censor/Canvas.jsx
@@ -1,18 +1,89 @@
 import React, { useRef, useEffect } from "react";
 
-const CanvasComponent = ({ addImage }) => {
+const CanvasComponent = ({ addImage, filter }) => {
   const canvasRef = useRef(null);
+  const image = new Image();
 
   useEffect(() => {
     const canvas = canvasRef.current;
+    if (!canvas) return;
     const context = canvas.getContext("2d");
-    const image = new Image();
-    image.src = addImage;
+    context.filter = "none";
 
+    canvas.width = canvas.parentElement.clientWidth;
+    canvas.height = canvas.parentElement.clientHeight;
     image.onload = () => {
       context.drawImage(image, 0, 0, canvas.width, canvas.height);
     };
   }, [addImage]);
+
+  image.src = addImage;
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    const rect = canvas.getBoundingClientRect();
+    // const rect = canvas.closest(".ReactModal__Content").getBoundingClientRect();
+    const mouse = {
+      x: 0,
+      y: 0,
+      click: false,
+    };
+
+    const radius = 50;
+
+    function draw(event) {
+      const rect = canvas.getBoundingClientRect();
+      const compressionPercentWidth = image.width / canvas.width;
+      const compressionPercentHeight = image.height / canvas.height;
+
+      const sourceMouseX =
+        (event.clientX - rect.left) * compressionPercentWidth;
+      const sourceMouseY =
+        (event.clientY - rect.top) * compressionPercentHeight;
+
+      mouse.x = event.clientX - rect.left;
+      mouse.y = event.clientY - rect.top;
+
+      if (filter == "blur") {
+        context.filter = "blur(2px)";
+      } else {
+        context.filter = "none";
+      }
+
+      context.drawImage(
+        image,
+        sourceMouseX - radius / 2,
+        sourceMouseY - radius / 2,
+        radius,
+        radius,
+        mouse.x - radius / 2 / compressionPercentWidth,
+        mouse.y - radius / 2 / compressionPercentHeight,
+        radius / compressionPercentWidth,
+        radius / compressionPercentHeight
+      );
+    }
+
+    canvas.addEventListener("mousedown", (event) => {
+      if (event.buttons === 1) {
+        mouse.click = true;
+      }
+      draw(event);
+    });
+
+    canvas.addEventListener("mousemove", (event) => {
+      if (mouse.click == true) {
+        draw(event);
+      }
+    });
+
+    canvas.addEventListener("mouseup", (event) => {
+      mouse.x = event.clientX - rect.left;
+      mouse.y = event.clientY - rect.top;
+      mouse.click = false;
+    });
+  }, [addImage, filter]);
 
   return (
     <div>
--- src/components/PopUps/CreateWork/Censor/Censor.jsx
+++ src/components/PopUps/CreateWork/Censor/Censor.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./censor.scss";
 
 import censorDefault from "@/img/censorDefault.jpg";
@@ -8,25 +8,46 @@ import stickerIcon from "@/img/icons/sticker-icon.svg";
 import CanvasComponent from "./Canvas";
 
 export default function Censor({ image }) {
+  const [clarity, setClarity] = useState(false);
+  const [blur, setBlur] = useState(false);
+
+  function choseBlur() {
+    setClarity(false);
+    setBlur(true);
+  }
+
+  function choseClarity() {
+    setClarity(true);
+    setBlur(false);
+  }
   return (
     <div className="censor">
       <h2 className="section-text_accent upload__title">
         Censor the public version of artwork, if it contains 18+ content
       </h2>
-      {/* <CanvasComponent addImage={image ? image : censorDefault} /> */}
-      <img
+      <CanvasComponent
+        addImage={image ? image : censorDefault}
+        filter={blur ? "blur" : clarity ? "clarity" : "none"}
+      />
+      {/* <img
         className="censor__image"
         src={image ? image : censorDefault}
         alt="created image"
-      />
+      /> */}
       <ul className="censor__icons">
         <li className="censor__icon">
           <img src={pictureIcon} alt="picture icon" />
         </li>
-        <li className="censor__icon">
+        <li
+          className={`censor__icon ${blur ? "active" : ""}`}
+          onClick={choseBlur}
+        >
           <img src={stickerIcon} alt="blur icon" />
         </li>
-        <li className="censor__icon">
+        <li
+          className={`censor__icon ${clarity ? "active" : ""}`}
+          onClick={choseClarity}
+        >
           <img src={blurIcon} alt="color select icon" />
         </li>
       </ul>
